d5ace98613cb0c52f5ba92f34a2a9cc19bc454e9
Fernanda Castilhos <fecastilhos@gmail.com>
2026-09-21 18:07:21 -0300

    Conecta entidades Venda e ItemVenda na rota GET /vendas

--- routes/vendas.dart
+++ routes/vendas.dart
@@ -1,29 +1,33 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:loja_estoque_api/models/item_venda.dart';
+import 'package:loja_estoque_api/models/venda.dart';
 
 Response onRequest(RequestContext context) {
   final vendas = [
-    {
-      'id': 1,
-      'data': '2026-09-16',
-      'forma_pagamento': 'pix',
-      'num_parcelas': null,
-      'valor_total': 49.90,
-      'itens': [
-        {'produto_codigo': '0001', 'valor_unitario': 49.90},
+    Venda(
+      id: 1,
+      data: '2026-09-16',
+      formaPagamento: 'pix',
+      numParcelas: null,
+      valorTotal: 49.90,
+      itens: [
+        ItemVenda(produtoCodigo: '0001', valorUnitario: 49.90),
       ],
-    },
-    {
-      'id': 2,
-      'data': '2026-09-16',
-      'forma_pagamento': 'credito_parcelado',
-      'num_parcelas': 3,
-      'valor_total': 179.80,
-      'itens': [
-        {'produto_codigo': '0001', 'valor_unitario': 49.90},
-        {'produto_codigo': '0003', 'valor_unitario': 129.90},
+    ),
+    Venda(
+      id: 2,
+      data: '2026-09-16',
+      formaPagamento: 'credito_parcelado',
+      numParcelas: 3,
+      valorTotal: 179.80,
+      itens: [
+        ItemVenda(produtoCodigo: '0001', valorUnitario: 49.90),
+        ItemVenda(produtoCodigo: '0003', valorUnitario: 129.90),
       ],
-    },
+    ),
   ];
 
-  return Response.json(body: vendas);
+  final vendasJson = vendas.map((venda) => venda.toJson()).toList();
+
+  return Response.json(body: vendasJson);
 }
